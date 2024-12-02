19cfa190da7d301512827e46f956a8e4fd7e2a94
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-02 18:24:33 +0100

    day 02

--- src/main/scala/se/johan1a/adventofcode2024/Day02.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day02.scala
@@ -2,11 +2,36 @@ package se.johan1a.adventofcode2024
 
 object Day02 {
 
-  def part1(input: Seq[String]): Int = {
-    -1
-  }
+  def part1(input: Seq[String]): Int = parse(input).filter(isSafe).size
+
+  private def isSafe(levels: Seq[Int]): Boolean =
+    if (levels(0) == levels(1)) {
+      false
+    } else {
+      val firstSign = sign(levels(0), levels(1))
+      levels
+        .sliding(2)
+        .map(ab => (ab(0), ab(1)))
+        .forall { (a, b) =>
+          val diff = (b - a).abs
+          diff >= 1 && diff <= 3 && sign(a, b) == firstSign
+        }
+    }
+
+  private def sign(a: Int, b: Int) = (b - a) / (b - a).abs
 
   def part2(input: Seq[String]): Int = {
-    -1
+    parse(input).filter { level =>
+      isSafe(level) || remove(level).exists(isSafe)
+    }.size
   }
+
+  private def remove(line: Seq[Int]): Seq[Seq[Int]] =
+    0.until(line.size).map { n =>
+      line.take(n) ++ line.drop(n + 1)
+    }
+
+  private def parse(input: Seq[String]) =
+    input.map(line => line.split("\\s+").map(_.toInt))
+
 }
--- src/test/scala/se/johan1a/adventofcode2024/Day02Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day02Test.scala
@@ -4,12 +4,20 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day02Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day02.part1(getInput("day02/test.txt")), 2)
+  }
+
   test("Part 1") {
-    assertEquals(Day02.part1(getInput("day02/input.txt")), -1)
+    assertEquals(Day02.part1(getInput("day02/input.txt")), 591)
+  }
+
+  test("Part 2 test") {
+    assertEquals(Day02.part2(getInput("day02/test.txt")), 4)
   }
 
   test("Part 2") {
-    assertEquals(Day02.part2(getInput("day02/input.txt")), -1)
+    assertEquals(Day02.part2(getInput("day02/input.txt")), 621)
   }
 
 }
