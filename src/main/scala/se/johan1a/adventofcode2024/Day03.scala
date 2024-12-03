cd8f776980910c802ba0119909c1a29ca6c51783
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-03 07:02:43 +0100

    day03

--- src/main/scala/se/johan1a/adventofcode2024/Day03.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day03.scala
@@ -1,12 +1,72 @@
 package se.johan1a.adventofcode2024
 
+import scala.util.Try
+
 object Day03 {
 
-  def part1(input: Seq[String]): Int = {
+  def part1(input: Seq[String]): Long = {
+    input.map(parse).sum
+  }
+
+  def part2(input: Seq[String]): Long = {
     -1
   }
 
-  def part2(input: Seq[String]): Int = {
-    -1
+  private def parse(line: String): Long = {
+    val chars = line.toCharArray
+    var i = 0
+    var sum = 0L
+    while (i < chars.size) {
+      tryParseMul(chars, i).map { (num, j) =>
+        sum += num
+        i = j
+      }.getOrElse {
+        i = i + 1
+      }
+    }
+    sum
+  }
+
+  private def tryParseMul(chars: Seq[Char], i: Int) =
+    for {
+      i0 <- tryParse(chars, i, "mul(")
+      (a, i1) <- tryParseNum(chars, i0)
+      i2 <- tryParse(chars, i1, ",")
+      (b, i3) <- tryParseNum(chars, i2)
+      i4 <- tryParse(chars, i3, ")")
+    } yield (a * b, i4)
+
+  private def tryParse(chars: Seq[Char], i: Int, s: String): Option[Int] = {
+    val res = tryParse(chars, i, s.toCharArray)
+    if (res.size == s.size) {
+      Some(i + s.size)
+    } else {
+      None
+    }
+  }
+
+  private def tryParse(chars: Seq[Char], i: Int, s: Seq[Char]): Seq[Char] = {
+    if (chars.size - i < s.size) {
+      Seq.empty
+    } else if (chars(i) == s.head) {
+      val res = chars(i)
+      if (s.size > 1) {
+        chars(i) +: tryParse(chars, i + 1, s.drop(1))
+      } else {
+        Seq(chars(i))
+      }
+    } else {
+      Seq.empty
+    }
+  }
+
+  private def tryParseNum(chars: Seq[Char], i: Int): Option[(Long, Int)] = {
+    var digits = Seq[Char]()
+    var j = i
+    while (j < chars.size && chars(j).isDigit && digits.size < 3) {
+      digits = digits :+ chars(j)
+      j += 1
+    }
+    Try(digits.mkString("").toLong).toOption.map(num => (num, j))
   }
 }
--- src/test/scala/se/johan1a/adventofcode2024/Day03Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day03Test.scala
@@ -4,12 +4,20 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day03Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day03.part1(getInput("day03/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day03.part1(getInput("day03/test.txt")), 161L)
   }
 
+  test("Part 1 test1") {
+    assertEquals(Day03.part1(getInput("day03/test1.txt")), 0L)
+  }
+
+   test("Part 1") {
+     assertEquals(Day03.part1(getInput("day03/input.txt")), 170778545)
+   }
+
   test("Part 2") {
-    assertEquals(Day03.part2(getInput("day03/input.txt")), -1)
+    assertEquals(Day03.part2(getInput("day03/input.txt")), -1L)
   }
 
 }
