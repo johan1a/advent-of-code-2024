ebf9836ab32cabacf358a85c303c951d71115425
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-05 08:51:39 +0100

    day05 part1

--- src/main/scala/se/johan1a/adventofcode2024/Day05.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day05.scala
@@ -1,9 +1,55 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.*
+
 object Day05 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+
+    val splitted = split(input)
+    val needs = parseRules(splitted.head)
+    println(needs)
+    val updates = parseUpdates(splitted.last)
+    val ok = updates.filter(valid(needs, _))
+
+    println(ok)
+    ok.map(getMiddle).sum
+  }
+
+  private def valid(allNeeds: Map[Int, Set[Int]], updates: Seq[Int]): Boolean = {
+    println(updates)
+
+    var done = Set[Int]()
+    updates.forall { n =>
+      val needs = allNeeds.getOrElse(n, Set.empty).filter(updates.contains)
+      if (needs.isEmpty || needs.forall(done.contains)) {
+        done = done + n
+        true
+      } else {
+        println(n)
+        false
+      }
+    }
+  }
+
+  private def getMiddle(updates: Seq[Int]) = {
+    updates(updates.size / 2)
+  }
+
+  private def parseRules(lines: Seq[String]) = {
+    var needs = Map[Int, Set[Int]]()
+    lines.map(l =>
+      val splitted = l.split("\\|")
+      val a = splitted.head.toInt
+      val b = splitted.last.toInt
+      val before = needs.getOrElse(b, Set.empty)
+      needs = needs + (b -> (before + a))
+    )
+    needs
+  }
+
+  private def parseUpdates(lines: Seq[String]): Seq[Seq[Int]] = {
+    lines.map(l => l.split(",").map(_.toInt))
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2024/Day05Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day05Test.scala
@@ -4,8 +4,12 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day05Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day05.part1(getInput("day05/test.txt")), 143)
+  }
+
   test("Part 1") {
-    assertEquals(Day05.part1(getInput("day05/input.txt")), -1)
+    assertEquals(Day05.part1(getInput("day05/input.txt")), 5452)
   }
 
   test("Part 2") {
