af4b98be32bd043c5fc7a8c4a6309bc8a936c79a
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-07 09:54:43 +0100

    day07 part 2

--- src/main/scala/se/johan1a/adventofcode2024/Day07.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day07.scala
@@ -11,7 +11,12 @@ object Day07:
       .sum
 
   def part2(input: Seq[String]): Long =
-    -1
+    input
+      .map(parse)
+      .map((n, nn) => calculate2(n, nn))
+      .filter(_._2)
+      .map(_._1)
+      .sum
 
   private def calculate(n: Long, numbers: Seq[Long]): (Long, Boolean) =
     val result = calculate(n, numbers.head, numbers.tail)
@@ -22,6 +27,21 @@ object Day07:
     else
       val next = remaining.head
       calculate(n, sum + next, remaining.tail) || calculate(n, sum * next, remaining.tail)
+
+  private def calculate2(n: Long, numbers: Seq[Long]): (Long, Boolean) =
+    val result = calculate2(n, numbers.head, numbers.tail)
+    (n, result)
+
+  private def calculate2(n: Long, sum: Long, remaining: Seq[Long]): Boolean =
+    if remaining.isEmpty then n == sum
+    else
+      val next = remaining.head
+      calculate2(n, sum + next, remaining.tail)
+      || calculate2(n, sum * next, remaining.tail)
+      || calculate2(n, combine(sum, next), remaining.tail)
+
+  def combine(a: Long, b: Long) =
+    (a.toString + b.toString).toLong
 
   private def parse(line: String) =
     val splitted = line.split(":").map(_.trim)
--- src/test/scala/se/johan1a/adventofcode2024/Day07Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day07Test.scala
@@ -12,6 +12,18 @@ class Day07Test extends munit.FunSuite:
     assertEquals(Day07.part1(getInput("day07/input.txt")), 66343330034722L)
   }
 
-  // test("Part 2") {
-  //   assertEquals(Day07.part2(getInput("day07/input.txt")), -1)
-  // }
+  test("Part 2 test") {
+    assertEquals(Day07.part2(getInput("day07/test.txt")), 11387L)
+  }
+
+  test("Part 2 test 1") {
+    assertEquals(Day07.part2(Seq("7290: 6 8 6 15 ")), 7290L)
+  }
+
+  test("Part 2 test 2") {
+    assertEquals(Day07.combine(48, 6), 486L)
+  }
+
+  test("Part 2") {
+    assertEquals(Day07.part2(getInput("day07/input.txt")), 637696070419031L)
+  }
