26b61c9f55509c3a852c2a51e8762f6692c0ef06
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-07 09:26:17 +0100

    day07 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day07.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day07.scala
@@ -2,8 +2,28 @@ package se.johan1a.adventofcode2024
 
 object Day07:
 
-  def part1(input: Seq[String]): Int =
+  def part1(input: Seq[String]): Long =
+    input
+      .map(parse)
+      .map((n, nn) => calculate(n, nn))
+      .filter(_._2)
+      .map(_._1)
+      .sum
+
+  def part2(input: Seq[String]): Long =
     -1
 
-  def part2(input: Seq[String]): Int =
-    -1
+  private def calculate(n: Long, numbers: Seq[Long]): (Long, Boolean) =
+    val result = calculate(n, numbers.head, numbers.tail)
+    (n, result)
+
+  private def calculate(n: Long, sum: Long, remaining: Seq[Long]): Boolean =
+    if remaining.isEmpty then n == sum
+    else
+      val next = remaining.head
+      calculate(n, sum + next, remaining.tail) || calculate(n, sum * next, remaining.tail)
+
+  private def parse(line: String) =
+    val splitted = line.split(":").map(_.trim)
+    val numbers = splitted.last.split("\\s").map(_.toLong)
+    (splitted.head.toLong, numbers)
--- src/test/scala/se/johan1a/adventofcode2024/Day07Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day07Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day07Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day07.part1(getInput("day07/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day07.part1(getInput("day07/test.txt")), 3749L)
   }
 
-  test("Part 2") {
-    assertEquals(Day07.part2(getInput("day07/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day07.part1(getInput("day07/input.txt")), 66343330034722L)
   }
+
+  // test("Part 2") {
+  //   assertEquals(Day07.part2(getInput("day07/input.txt")), -1)
+  // }
