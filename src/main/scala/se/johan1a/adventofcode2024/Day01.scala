ab61756ef2b0c66539b681b3e8a1604563d38721
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-08 12:19:20 +0100

    refactor day01

--- src/main/scala/se/johan1a/adventofcode2024/Day01.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day01.scala
@@ -3,18 +3,19 @@ package se.johan1a.adventofcode2024
 object Day01:
 
   def part1(input: Seq[String]): Int =
-    val splitted = input.map(line => line.split("\\s+"))
-    val left = splitted.map(pair => pair.head.toInt).sorted
-    val right = splitted.map(pair => pair.last.toInt).sorted
-
+    val (left, right) = parse(input)
     left
       .zip(right)
       .map((a, b) => (a - b).abs)
       .sum
 
   def part2(input: Seq[String]): Int =
-    val splitted = input.map(line => line.split("\\s+"))
-    val left = splitted.map(pair => pair.head.toInt)
-    val right = splitted.map(pair => pair.last.toInt)
-    val counts = right.groupBy(identity).mapValues(_.size)
+    val (left, right) = parse(input)
+    val counts = right.groupBy(identity).view.mapValues(_.size)
     left.map(n => n * counts.getOrElse(n, 0)).sum
+
+  private def parse(input: Seq[String]) =
+    val splitted = input.map(line => line.split("\\s+"))
+    val left = splitted.map(pair => pair.head.toInt).sorted
+    val right = splitted.map(pair => pair.last.toInt).sorted
+    (left, right)
