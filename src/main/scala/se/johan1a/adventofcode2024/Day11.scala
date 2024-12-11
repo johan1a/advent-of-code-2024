d0845fce6ca78c1aecc080ced2ab60c79e0d9942
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-11 21:22:55 +0100

    day11 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day11.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day11.scala
@@ -1,9 +1,27 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.*
+
 object Day11:
 
-  def part1(input: Seq[String]): Int =
-    -1
+  def part1(input: Seq[String], n: Int = 25): Long =
+    var stones: Seq[Long] = input.head.split("\\s").map(_.toString.toLong)
+    0.until(n).foreach { _ =>
+      val newStones = stones.flatMap { stone =>
+        if stone == 0 then
+          Seq(1L)
+        else
+          val digits = stone.toString
+          if digits.length % 2 == 0 then
+            val a = digits.take(digits.length / 2).toLong
+            val b = digits.drop(digits.length / 2).toLong
+            Seq(a, b)
+          else
+            Seq(stone * 2024L)
+      }
+      stones = newStones
+    }
+    stones.size
 
-  def part2(input: Seq[String]): Int =
+  def part2(input: Seq[String]): Long =
     -1
--- src/test/scala/se/johan1a/adventofcode2024/Day11Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day11Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day11Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day11.part1(getInput("day11/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day11.part1(getInput("day11/test.txt")), 55312L)
   }
 
-  test("Part 2") {
-    assertEquals(Day11.part2(getInput("day11/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day11.part1(getInput("day11/input.txt")), 186175L)
   }
+
+//  test("Part 2") {
+//    assertEquals(Day11.part2(getInput("day11/input.txt")), -1L)
+//  }
