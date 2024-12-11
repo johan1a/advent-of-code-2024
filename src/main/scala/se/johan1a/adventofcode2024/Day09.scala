d4c0c5d9fb28e232d9d9a910a00f3137a96c7809
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-11 18:28:33 +0100

    day09 part 2 refactor

--- src/main/scala/se/johan1a/adventofcode2024/Day09.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day09.scala
@@ -3,10 +3,10 @@ package se.johan1a.adventofcode2024
 object Day09:
 
   def part1(input: Seq[String]): Long =
-    val line = input.head.toCharArray().map(_.toString.toInt)
+    val line = input.head.toCharArray.map(_.toString.toInt)
     var sum = 0L
     var i = 0
-    var j = line.size - 1
+    var j = line.length - 1
     var pos = 0
     while i <= j do
       if i % 2 == 0 then
@@ -30,19 +30,19 @@ object Day09:
     sum
 
   case class File(id: Int, size: Int)
+
   case class Block(size: Int, files: Seq[File])
 
   def part2(input: Seq[String]): Long =
     val blocks = parse(input.head)
-    //printBlocks(blocks)
-    val nbrBlocks = blocks.size
+    val nbrBlocks = blocks.length
     var i = 0
-    var j = blocks.size - 1
+    var j = blocks.length - 1
     while i <= j do
       var k = i
       assert(blocks(j).files.size == 1)
       val fileToMove = blocks(j).files.head
-      while k < blocks.size && !fits(blocks(k), fileToMove) do
+      while k < blocks.length && !fits(blocks(k), fileToMove) do
         k += 1
       if k < j then
         put(blocks, k, j)
@@ -50,29 +50,22 @@ object Day09:
       if freeSpace(blocks(i)) == 0 then
         i += 1
 
-    //printBlocks(blocks)
     count(blocks)
 
   private def count(blocks: Array[Block]): Long =
-    var i = 0
     var pos = 0
     var sum = 0L
-    while i < blocks.size do
-      var j = 0
+    blocks.indices.foreach: i =>
       val block = blocks(i)
-      while j < block.files.size do
-        var k = 0
+      block.files.indices.foreach: j =>
         val file = block.files(j)
-        while k < file.size do
+        0.until(file.size).foreach: _ =>
           sum += pos * file.id
           pos += 1
-          k += 1
-        j += 1
       pos += freeSpace(block)
-      i += 1
     sum
 
-  private def put(blocks: Array[Block], dest: Int, src: Int) =
+  private def put(blocks: Array[Block], dest: Int, src: Int): Unit =
     val fileToMove: File = blocks(src).files.head
     val prevDestBlock = blocks(dest)
     val newDestBlock = Block(prevDestBlock.size, prevDestBlock.files :+ fileToMove)
@@ -86,24 +79,6 @@ object Day09:
     assert(result >= 0)
     result
 
-  private def printBlocks(blocks: Array[Day09.Block]) =
-    var i = 0
-    while i < blocks.size do
-      var j = 0
-      val block = blocks(i)
-      while j < block.files.size do
-        var k = 0
-        val file = block.files(j)
-        while k < file.size do
-          print(file.id)
-          k += 1
-        j += 1
-      0.until(freeSpace(block)).foreach { _ =>
-        print(".")
-      }
-      i += 1
-    println()
-
   private def parse(input: String): Array[Block] =
     input.toCharArray.zipWithIndex.map { (char, i) =>
       val size = char.toString.toInt
@@ -112,4 +87,4 @@ object Day09:
         Block(size, Seq(File(id, size)))
       else
         Block(size, Seq.empty)
-    }.toArray
+    }
