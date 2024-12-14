21b11576c5ba38b5c51eb28d5a21ebf3e7f37633
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-14 16:28:29 +0100

    day13 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day13.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day13.scala
@@ -1,9 +1,56 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.*
+
 object Day13:
 
+  case class Machine(a: Vec2, b: Vec2, prize: Pos)
+
+  var cache: Map[(Vec2, Int), Option[Int]] = Map()
+
   def part1(input: Seq[String]): Int =
-    -1
+    parse(input).map(machine =>
+      cache = Map()
+      best(machine)
+    ).map(_.getOrElse(0)).sum
 
   def part2(input: Seq[String]): Int =
     -1
+
+  private def best(
+      machine: Machine,
+      pos: Pos = Vec2(0, 0),
+      cost: Int = 0,
+      nbrAPresses: Int = 0,
+      nbrBPresses: Int = 0
+  ): Option[Int] =
+    if cache.contains((pos, cost)) then
+      cache((pos, cost))
+    else
+      val result = if nbrAPresses > 100 || nbrBPresses > 100 || pos.x > machine.prize.x || pos.y > machine.prize.y then
+        None
+      else if pos == machine.prize then
+        println(s"found solution with cost $cost, $nbrAPresses a presses, $nbrBPresses bPresses")
+        Some(cost)
+      else
+        val pressA = best(machine, pos + machine.a, 3 + cost, nbrAPresses + 1, nbrBPresses)
+        val pressB = best(machine, pos + machine.b, 1 + cost, nbrAPresses, nbrBPresses + 1)
+        (pressA, pressB) match
+          case (Some(a), Some(b)) => Some(Math.min(a, b))
+          case (Some(a), None)    => Some(a)
+          case (None, Some(b))    => Some(b)
+          case _                  => None
+
+      cache = cache + ((pos, cost) -> result)
+      result
+
+  private def parse(input: Seq[String]): Seq[Machine] =
+    split(input).map { lines =>
+      val a = numbers(lines(0))
+      val aDiff = Vec2(a.head, a.last)
+      val b = numbers(lines(1))
+      val bDiff = Vec2(b.head, b.last)
+      val prize = numbers(lines(2))
+      val prizePos = Vec2(prize.head, prize.last)
+      Machine(aDiff, bDiff, prizePos)
+    }
--- src/test/scala/se/johan1a/adventofcode2024/Day13Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day13Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day13Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day13.part1(getInput("day13/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day13.part1(getInput("day13/test.txt")), 480)
   }
 
-  test("Part 2") {
-    assertEquals(Day13.part2(getInput("day13/input.txt")), -1)
-  }
+   test("Part 1") {
+     assertEquals(Day13.part1(getInput("day13/input.txt")), 36758)
+   }
+
+  // test("Part 2") {
+  //   assertEquals(Day13.part2(getInput("day13/input.txt")), -1)
+  // }
