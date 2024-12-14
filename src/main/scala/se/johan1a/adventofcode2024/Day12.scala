f1a0707d69b86c7b17a79cdd2e65147831613437
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-14 14:08:45 +0100

    day12  refactor

--- src/main/scala/se/johan1a/adventofcode2024/Day12.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day12.scala
@@ -11,39 +11,32 @@ object Day12:
     val grid = makeGrid(input)
     seen = Set[Pos]()
     allPositions(grid).filterNot(seen.contains).map(pos =>
-      val (area, perimiter, nbrPerimiter, _) = calculate(grid, pos)
-      area * nbrPerimiter
+      val (area, perimeter, nbrPerimeter) = calculate(grid, pos)
+      area * nbrPerimeter
     ).sum
 
-  private def calculate(grid: Utils.Grid, start: Vec2): (Int, Set[Pos], Int, Set[Pos]) =
+  private def calculate(grid: Utils.Grid, start: Vec2): (Int, Set[Pos], Int) =
     var queue = Seq(start)
     var area = 0
-    var perimiter = Set[Pos]()
-    var nbrPerimiter = 0
+    var perimeter = Set[Pos]()
+    var nbrPerimeter = 0
     val plantType = get(grid, start)
-    var localGroup = Set[Pos]()
     while queue.nonEmpty do
       val pos = queue.head
       queue = queue.tail
       if !seen.contains(pos) then
         seen = seen + pos
-        localGroup = localGroup + pos
         area += 1
 
-        val below = Vec2(0, 1) + pos
-        val above = Vec2(0, -1) + pos
-        val rightOf = Vec2(1, 0) + pos
-        val leftOf = Vec2(-1, 0) + pos
-
-        val neighbors = Seq(below, above, rightOf, leftOf)
-        neighbors.foreach(neighbor =>
-          if getOpt(grid, neighbor).contains(plantType) then
-            queue = queue :+ neighbor
-          else if isPerimiter(grid, plantType, neighbor) then
-            perimiter = perimiter + neighbor
-            nbrPerimiter += 1
-        )
-    (area, perimiter, nbrPerimiter, localGroup)
+        Seq(Vec2(0, 1), Vec2(0, -1), Vec2(1, 0), Vec2(-1, 0))
+          .map(d => pos + d).foreach(neighbor =>
+            if getOpt(grid, neighbor).contains(plantType) then
+              queue = queue :+ neighbor
+            else if isPerimiter(grid, plantType, neighbor) then
+              perimeter = perimeter + neighbor
+              nbrPerimeter += 1
+          )
+    (area, perimeter, nbrPerimeter)
 
   private def isPerimiter(grid: Grid, plantType: Char, neighbor: Pos) =
     !inRange(grid, neighbor) || get(grid, neighbor) != plantType
@@ -52,12 +45,8 @@ object Day12:
     val grid = makeGrid(input)
     seen = Set[Pos]()
     allPositions(grid).filterNot(seen.contains).map(pos =>
-      // also for each hole, go around it inside. keep track of visited holes
-      // add outline to hole nbr sides
-      val (area, perimiter, _, group) = calculate(grid, pos)
+      val (area, perimiter, _) = calculate(grid, pos)
       if area > 0 then
-
-        val topLeft = group.toSeq.minBy(p => (p.y, p.x))
         val plantType = get(grid, pos)
         val nbrSides = getNbrSides(grid, perimiter, plantType)
 
@@ -66,11 +55,11 @@ object Day12:
         0
     ).sum
 
-  private def getNbrSides(grid: Grid, perimiter: Set[Pos], plantType: Char) =
+  private def getNbrSides(grid: Grid, perimeter: Set[Pos], plantType: Char) =
     var seen = Set[(Pos, Dir)]()
     var nbrSides = 0
     var first = true
-    perimiter
+    perimeter
       .toSeq
       .sortBy(p => (p.y, p.x))
       .foreach { start =>
@@ -85,9 +74,9 @@ object Day12:
 
             val nextPosInFront = pos + dir
             val nextPosToTheRight = getNextToTheRight(grid, pos, dir)
-            val nextInFront = perimiter.contains(nextPosInFront)
-            val nextRight = perimiter.contains(nextPosToTheRight)
-            val rightOf = perimiter.contains(getPosToTheRight(grid, pos, dir))
+            val nextInFront = perimeter.contains(nextPosInFront)
+            val nextRight = perimeter.contains(nextPosToTheRight)
+            val rightOf = perimeter.contains(getPosToTheRight(grid, pos, dir))
 
             (nextInFront, nextRight, rightOf) match
               case (_, true, false) =>
@@ -105,11 +94,6 @@ object Day12:
                 pos = pos + dir
       }
     nbrSides
-
-  private def getSame(grid: Grid, originalPlantType: Char, pos: Vec2) =
-    getOpt(grid, pos) match
-      case Some(plantType) if plantType == originalPlantType => Some(plantType)
-      case _                                                 => None
 
   private def getNextToTheRight(grid: Grid, pos: Pos, dir: Dir) =
     val inFront = pos + dir
