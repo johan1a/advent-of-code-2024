0266f6e736f339a3440a3a7516369707a8e43fce
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-15 13:30:25 +0100

    day14 part 2

--- src/main/scala/se/johan1a/adventofcode2024/Day14.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day14.scala
@@ -1,6 +1,6 @@
 package se.johan1a.adventofcode2024
 
-import se.johan1a.adventofcode2024.Utils.{Vec2, numbers, printGrid}
+import se.johan1a.adventofcode2024.Utils.{Vec2, numbers}
 
 object Day14:
 
@@ -13,7 +13,7 @@ object Day14:
     }
     safetyFactor(robots, w, h)
 
-  private def simulate(robots: Set[Robot], w: Int, h: Int) =
+  private def simulate(robots: Seq[Robot], w: Int, h: Int) =
     robots.map { robot =>
       val pos = Vec2(
         wrap(robot.p.x, robot.v.x, w),
@@ -25,7 +25,7 @@ object Day14:
   private def wrap(p: Long, v: Long, w: Int) =
     (p + v + w) % w
 
-  private def safetyFactor(robots: Set[Robot], w: Int, h: Int) =
+  private def safetyFactor(robots: Seq[Robot], w: Int, h: Int) =
     val quadrants = Array(0, 0, 0, 0)
     val midX = w / 2
     val midY = h / 2
@@ -41,20 +41,55 @@ object Day14:
     }
     quadrants.product
 
+  private def move(robot: Robot, n: Int, w: Int, h: Int): Robot =
+    Robot(
+      Vec2(
+        wrap(robot.p.x, n * robot.v.x, w),
+        wrap(robot.p.y, n * robot.v.y, h)
+      ),
+      robot.v
+    )
+
   def part2(input: Seq[String], t: Int = 10000000, w: Int = 101, h: Int = 103): Int =
     var robots = parse(input)
     var i = 0
-    var result: Option[Int] = None
-    while i < t && result.isEmpty do
-      if i % 10000 == 0 then
+    var hasResult = false
+    while i < t && !hasResult do
+      if i % 1000 == 0 then
         println(s"i: $i")
-        //printRobots(robots, w, h)
       robots = simulate(robots, w, h)
-      result = count(robots, i, w, h)
+      hasResult = findTree(robots, w, h)
       i += 1
-    result.getOrElse(-2)
 
-  private def count(robots: Set[Robot], i: Int, w: Int, h: Int): Option[Int] =
+    i
+// >30000
+  // tried over 1637000
+  private def findTree(robots: Seq[Day14.Robot], w: Int, h: Int) =
+    val positions = robots.map(_.p).toSet
+    var result = false
+    var rows = Seq[String]()
+    0.until(h).foreach { y =>
+      val string = 0.until(w).map { x =>
+        if positions.contains(Vec2(x, y)) then
+          "#"
+        else
+          "."
+      }.mkString("")
+      rows = rows :+ string
+      if string.contains("########") then
+        result = true
+    }
+    if result then
+      rows.foreach(println)
+    result
+
+  private def allHaveInverted(robots: Seq[Robot], i: Int, i1: Int, i2: Int) =
+    ???
+
+  private def getX(robot: Day14.Robot, robot1: Day14.Robot, i: Int, i1: Int) =
+    ???
+
+  private def count(robots: Seq[Robot], i: Int, w: Int, h: Int): Option[Int] =
     val positions = robots.map(_.p)
     val quadrants = Array(0, 0, 0, 0)
     val midX = w / 2
@@ -86,7 +121,7 @@ object Day14:
       printRobots(robots, w, h)
     result
 
-  private def printRobots(robots: Set[Robot], w: Int, h: Int): Unit =
+  private def printRobots(robots: Seq[Robot], w: Int, h: Int): Unit =
     val positions = robots.map(_.p)
     0.until(h).foreach { y =>
       0.until(w).foreach { x =>
@@ -98,8 +133,8 @@ object Day14:
       println()
     }
 
-  private def parse(input: Seq[String]): Set[Robot] =
+  private def parse(input: Seq[String]): Seq[Robot] =
     input.map { line =>
       val nn = numbers(line)
       Robot(Vec2(nn(0), nn(1)), Vec2(nn(2), nn(3)))
-    }.toSet
+    }
--- src/test/scala/se/johan1a/adventofcode2024/Day14Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day14Test.scala
@@ -13,5 +13,5 @@ class Day14Test extends munit.FunSuite:
   }
 
   test("Part 2") {
-    assertEquals(Day14.part2(getInput("day14/input.txt")), -1)
+    assertEquals(Day14.part2(getInput("day14/input.txt")), 6243)
   }
