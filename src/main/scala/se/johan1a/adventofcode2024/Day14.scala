4d460c92022337170a8d8937e3dae37fafbcf16d
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-15 13:32:10 +0100

    day14 refactor

--- src/main/scala/se/johan1a/adventofcode2024/Day14.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day14.scala
@@ -41,15 +41,6 @@ object Day14:
     }
     quadrants.product
 
-  private def move(robot: Robot, n: Int, w: Int, h: Int): Robot =
-    Robot(
-      Vec2(
-        wrap(robot.p.x, n * robot.v.x, w),
-        wrap(robot.p.y, n * robot.v.y, h)
-      ),
-      robot.v
-    )
-
   def part2(input: Seq[String], t: Int = 10000000, w: Int = 101, h: Int = 103): Int =
     var robots = parse(input)
     var i = 0
@@ -60,10 +51,8 @@ object Day14:
       robots = simulate(robots, w, h)
       hasResult = findTree(robots, w, h)
       i += 1
-
     i
-// >30000
-  // tried over 1637000
+
   private def findTree(robots: Seq[Day14.Robot], w: Int, h: Int) =
     val positions = robots.map(_.p).toSet
     var result = false
@@ -82,56 +71,6 @@ object Day14:
     if result then
       rows.foreach(println)
     result
-
-  private def allHaveInverted(robots: Seq[Robot], i: Int, i1: Int, i2: Int) =
-    ???
-
-  private def getX(robot: Day14.Robot, robot1: Day14.Robot, i: Int, i1: Int) =
-    ???
-
-  private def count(robots: Seq[Robot], i: Int, w: Int, h: Int): Option[Int] =
-    val positions = robots.map(_.p)
-    val quadrants = Array(0, 0, 0, 0)
-    val midX = w / 2
-    val midY = h / 2
-    var result: Option[Int] = Some(i)
-    var queue = robots
-    while result.isDefined do
-      val robot = queue.head
-      queue = queue.tail
-      if robot.p.x < midX && robot.p.y < midY then
-        quadrants(0) += 1
-        val x2 = w - 1 - robot.p.x
-        if !positions.contains(Vec2(x2, robot.p.y)) then
-          result = None
-      else if robot.p.x > midX && robot.p.y < midY then
-        quadrants(1) += 1
-      else if robot.p.x < midX && robot.p.y > midY then
-        quadrants(2) += 1
-        val x2 = w - 1 - robot.p.x
-        if !positions.contains(Vec2(x2, robot.p.y)) then
-          result = None
-      else if robot.p.x > midX && robot.p.y > midY then
-        quadrants(3) += 1
-
-//    if quadrants(0) == quadrants(1) && quadrants(2) == quadrants(3) then
-//      println(s"same at $i")
-
-    if result.isDefined then
-      printRobots(robots, w, h)
-    result
-
-  private def printRobots(robots: Seq[Robot], w: Int, h: Int): Unit =
-    val positions = robots.map(_.p)
-    0.until(h).foreach { y =>
-      0.until(w).foreach { x =>
-        if positions.contains(Vec2(x, y)) then
-          print("X")
-        else
-          print(".")
-      }
-      println()
-    }
 
   private def parse(input: Seq[String]): Seq[Robot] =
     input.map { line =>
