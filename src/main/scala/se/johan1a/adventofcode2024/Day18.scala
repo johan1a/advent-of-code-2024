5682c4eeb89214c7010f6895646017ac2fb6b2ec
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-20 15:16:47 +0100

    day18 part 2

--- src/main/scala/se/johan1a/adventofcode2024/Day18.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day18.scala
@@ -6,7 +6,6 @@ object Day18:
 
   var cache = Map[(Pos, Int), Option[Int]]()
 
-  // only works through sbt due to xss / max stack size
   def part1(input: Seq[String], w: Int = 71, n: Int = 1024): Int =
     cache = Map()
     val bytes = parse(input).take(n).toSet
@@ -14,8 +13,25 @@ object Day18:
     val target = Vec2(w - 1, w - 1)
     shortestPath(bytes, w, Set.empty, start, target, 0).get
 
-  def part2(input: Seq[String]): Int =
-    -1
+  def part2(input: Seq[String], w: Int = 71, lowStart: Int = 1024): Vec2 =
+    val bytes = parse(input)
+    val start = Vec2(0, 0)
+    val target = Vec2(w - 1, w - 1)
+    var low = lowStart
+    var high = bytes.size - 1
+    while low <= high do
+      val mid = (high - low) / 2 + low
+      cache = Map()
+      val beforeMidVal = shortestPath(bytes.take(mid - 1).toSet, w, Set.empty, start, target, 0)
+      cache = Map()
+      val midVal = shortestPath(bytes.take(mid).toSet, w, Set.empty, start, target, 0)
+      if beforeMidVal.isDefined && midVal.isEmpty then
+        return bytes(mid - 1)
+      else if beforeMidVal.isEmpty && midVal.isEmpty then
+        high = mid - 1
+      else
+        low = mid + 1
+    Vec2(-1, -1)
 
   private def shortestPath(bytes: Set[Pos], w: Int, seen: Set[Pos], pos: Pos, target: Pos, dist: Int): Option[Int] =
     if pos == target then
--- src/test/scala/se/johan1a/adventofcode2024/Day18Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day18Test.scala
@@ -1,6 +1,7 @@
 package se.johan1a.adventofcode2024
 
 import se.johan1a.adventofcode2024.TestInputUtil.getInput
+import se.johan1a.adventofcode2024.Utils.Vec2
 
 class Day18Test extends munit.FunSuite:
 
@@ -8,10 +9,15 @@ class Day18Test extends munit.FunSuite:
     assertEquals(Day18.part1(getInput("day18/test.txt"), w = 7, n = 12), 22)
   }
 
-   test("Part 1") {
-     assertEquals(Day18.part1(getInput("day18/input.txt")), 348)
-   }
+  // To run in intellij, Modify run configuration, add under VM options: -Xss200m
+  test("Part 1") {
+    assertEquals(Day18.part1(getInput("day18/input.txt")), 348)
+  }
 
-  // test("Part 2") {
-  //   assertEquals(Day18.part2(getInput("day18/input.txt")), -1)
-  // }
+  test("Part 2 test") {
+    assertEquals(Day18.part2(getInput("day18/test.txt"), w = 7, lowStart = 0), Vec2(6, 1))
+  }
+
+  test("Part 2") {
+    assertEquals(Day18.part2(getInput("day18/input.txt")), Vec2(54, 44))
+  }
