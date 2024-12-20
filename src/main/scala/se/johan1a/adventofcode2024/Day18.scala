b66928a8a6d8ac9a548a489c916962d74e1bad54
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-20 14:21:17 +0100

    day18 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day18.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day18.scala
@@ -1,9 +1,37 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.*
+
 object Day18:
 
-  def part1(input: Seq[String]): Int =
-    -1
+  var cache = Map[(Pos, Int), Option[Int]]()
+
+  // only works through sbt due to xss / max stack size
+  def part1(input: Seq[String], w: Int = 71, n: Int = 1024): Int =
+    cache = Map()
+    val bytes = parse(input).take(n).toSet
+    val start = Vec2(0, 0)
+    val target = Vec2(w - 1, w - 1)
+    shortestPath(bytes, w, Set.empty, start, target, 0).get
 
   def part2(input: Seq[String]): Int =
     -1
+
+  private def shortestPath(bytes: Set[Pos], w: Int, seen: Set[Pos], pos: Pos, target: Pos, dist: Int): Option[Int] =
+    if pos == target then
+      Some(dist)
+    else
+      if cache.contains((pos, dist)) then
+        cache((pos, dist))
+      else
+        val neighbors = Utils.neighbors(pos, min = Vec2(0, 0), max = Vec2(w, w), includeDiagonals = false)
+          .filter(n => !seen.contains(n) && !bytes.contains(n))
+        val result = neighbors.flatMap(n => shortestPath(bytes, w, seen + n, n, target, dist + 1)).minOption
+        cache = cache + ((pos, dist) -> result)
+        result
+
+  private def parse(input: Seq[String]): Seq[Pos] =
+    input.map { line =>
+      val splitted = numbers(line)
+      Vec2(splitted.head, splitted.last)
+    }
--- src/test/scala/se/johan1a/adventofcode2024/Day18Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day18Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day18Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day18.part1(getInput("day18/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day18.part1(getInput("day18/test.txt"), w = 7, n = 12), 22)
   }
 
-  test("Part 2") {
-    assertEquals(Day18.part2(getInput("day18/input.txt")), -1)
-  }
+   test("Part 1") {
+     assertEquals(Day18.part1(getInput("day18/input.txt")), 348)
+   }
+
+  // test("Part 2") {
+  //   assertEquals(Day18.part2(getInput("day18/input.txt")), -1)
+  // }
