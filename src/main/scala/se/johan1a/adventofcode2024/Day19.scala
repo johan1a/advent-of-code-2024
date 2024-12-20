d1695727c00d6d8a8513c1f177fae1bb077c43b2
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-20 15:29:19 +0100

    day19 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day19.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day19.scala
@@ -1,9 +1,33 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.split
+
 object Day19:
 
+  var cache = Map[String, Boolean]()
+
   def part1(input: Seq[String]): Int =
-    -1
+    cache = Map()
+    val (towels, designs) = parse(input)
+    designs.count(isPossible(towels, _))
 
   def part2(input: Seq[String]): Int =
     -1
+
+  private def isPossible(towels: Set[String], design: String): Boolean =
+    if cache.contains(design) then
+      cache(design)
+    else if design.isEmpty then
+      true
+    else
+      val result = towels.exists { towel =>
+        design.startsWith(towel) && isPossible(towels, design.drop(towel.length))
+      }
+      cache = cache + (design -> result)
+      result
+
+  private def parse(input: Seq[String]): (Set[String], Seq[String]) =
+    val splitted = split(input)
+    val towels = splitted.head.head.split(",").map(_.trim).toSet
+    val designs = splitted.last
+    (towels, designs)
--- src/test/scala/se/johan1a/adventofcode2024/Day19Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day19Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day19Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day19.part1(getInput("day19/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day19.part1(getInput("day19/test.txt")), 6)
   }
 
-  test("Part 2") {
-    assertEquals(Day19.part2(getInput("day19/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day19.part1(getInput("day19/input.txt")), 338)
   }
+
+//  test("Part 2") {
+//    assertEquals(Day19.part2(getInput("day19/input.txt")), -1)
+//  }
