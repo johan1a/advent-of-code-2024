ec74388020793f29488dd929eb31e882019aaa0c
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-25 11:27:58 +0100

    day22 part 1

--- src/main/scala/se/johan1a/adventofcode2024/Day22.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day22.scala
@@ -1,9 +1,35 @@
 package se.johan1a.adventofcode2024
 
+import scala.annotation.tailrec
+
 object Day22:
 
-  def part1(input: Seq[String]): Int =
+  def part1(input: Seq[String]): Long =
+    val results = input.map(str => getSecretValue(str.toLong, 2000))
+    results.sum
+
+  def part2(input: Seq[String]): Long =
     -1
 
-  def part2(input: Seq[String]): Int =
-    -1
+  @tailrec
+  private def getSecretValue(original: Long, n: Int): Long =
+    if n == 0 then
+      original
+    else
+      var secret = original
+      val a = secret * 64
+      secret = mix(a, secret)
+      secret = prune(secret)
+      val c = secret / 32
+      secret = mix(c, secret)
+      secret = prune(secret)
+      val d = secret * 2048
+      secret = mix(d, secret)
+      secret = prune(secret)
+      getSecretValue(secret, n - 1)
+
+  private def mix(n: Long, secret: Long): Long =
+    n ^ secret
+
+  private def prune(secret: Long): Long =
+    secret % 16777216L
--- src/test/scala/se/johan1a/adventofcode2024/Day22Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day22Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day22Test extends munit.FunSuite:
 
-  test("Part 1") {
-    assertEquals(Day22.part1(getInput("day22/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day22.part1(getInput("day22/test.txt")), 37327623L)
   }
 
-  test("Part 2") {
-    assertEquals(Day22.part2(getInput("day22/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day22.part1(getInput("day22/input.txt")), 13753970725L)
   }
+
+//  test("Part 2") {
+//    assertEquals(Day22.part2(getInput("day22/input.txt")), -1L)
+//  }
