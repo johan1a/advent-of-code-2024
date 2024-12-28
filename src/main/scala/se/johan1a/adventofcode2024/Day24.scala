8bc041cd053a3391516fac758bcecaa90fcdae4f
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-28 14:06:29 +0100

    day24 part 2 wip

--- src/main/scala/se/johan1a/adventofcode2024/Day24.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day24.scala
@@ -1,6 +1,7 @@
 package se.johan1a.adventofcode2024
 
 import se.johan1a.adventofcode2024.Utils.{split, splitOnce}
+import scala.collection.mutable
 
 object Day24:
 
@@ -18,8 +19,61 @@ object Day24:
     val bits = zRefs.map(ref => compute(ops, ref)).map(b => if b then "1" else "0").mkString
     java.lang.Long.parseLong(bits, 2)
 
-  def part2(input: Seq[String]): Int =
-    -1
+  def part2(input: Seq[String], n: Int = 4): String =
+    val opSeq = parse(input)
+    val refs = opSeq.map(_.ref)
+    val ops: mutable.Map[String, Op] = mutable.Map[String, Op]() // opSeq.map(op => op.ref -> op).toMap
+    opSeq.foreach { op =>
+      ops.put(op.ref, op)
+    }
+    val zRefs = refs.filter(_.startsWith("z")).sorted.reverse
+    val x = parseLiterals(opSeq, "x")
+    val y = parseLiterals(opSeq, "y")
+    findFlipped(ops, zRefs, x, y, n).flatMap(tuple => Seq(tuple._1, tuple._2)).sorted.mkString(",")
+
+  private def put(ops: mutable.Map[String, Op], k: Long, prefix: String): Unit =
+    val refs = ops.keys.toSeq.filter(_.startsWith(prefix))
+    refs.foreach(ref =>
+      ops.remove(ref)
+    )
+    val kBits = getPrefixedBits(k, prefix)
+    kBits.foreach { (ref, value) =>
+      ops.put(ref, Literal(ref, value == "1"))
+    }
+
+  def getPrefixedBits(k: Long, prefix: String): Seq[(String, String)] =
+    k.toBinaryString.reverse.zipWithIndex.map { case (char, i) => getRef(i, prefix) -> char.toString }
+
+  private def parseLiterals(ops: Seq[Op], prefix: String): Long =
+    java.lang.Long.parseLong(
+      ops.sortBy(_.ref).reverse.filter(_.ref.startsWith(prefix)).collect { case Literal(ref, value) =>
+        if value then "1" else "0"
+      }.mkString,
+      2
+    )
+
+  private def findFlipped(
+      ops: mutable.Map[String, Op],
+      zRefs: Seq[String],
+      x: Long,
+      y: Long,
+      n: Int
+  ): Seq[(String, String)] =
+    val expectedBits = getPrefixedBits(x + y, "z")
+    val bits = computeToBits(ops.toMap, zRefs)
+    val sum = java.lang.Long.parseLong(bits, 2)
+    sum == x + y
+    Seq.empty
+
+  private def computeToBits(ops: Map[String, Op], zRefs: Seq[String]) =
+    zRefs.map(ref => compute(ops, ref)).map(b => if b then "1" else "0").mkString
+
+  // 001001
+  private def getRef(i: Int, prefix: String) =
+    if i <= 9 then
+      s"${prefix}0$i"
+    else
+      s"$prefix$i"
 
   private def compute(refs: Map[String, Op], ref: String): Boolean =
     refs(ref) match
--- src/test/scala/se/johan1a/adventofcode2024/Day24Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day24Test.scala
@@ -16,6 +16,31 @@ class Day24Test extends munit.FunSuite:
     assertEquals(Day24.part1(getInput("day24/input.txt")), 36902370467952L)
   }
 
+  test("Part 2 test0") {
+    assertEquals(Day24.part2(getInput("day24/test2.txt"), n = 2), "z00,z01,z02,z05")
+  }
+
+  test("Part 2 test1") {
+    assertEquals(
+      Day24.getPrefixedBits(5, "z"),
+      Seq(
+        ("z00", "1"),
+        ("z01", "0"),
+        ("z02", "1")
+      )
+    )
+
+    assertEquals(
+      Day24.getPrefixedBits(11, "x"),
+      Seq(
+        ("x00", "1"),
+        ("x01", "1"),
+        ("x02", "0"),
+        ("x03", "1")
+      )
+    )
+  }
+
   test("Part 2") {
-    assertEquals(Day24.part2(getInput("day24/input.txt")), -1)
+    assertEquals(Day24.part2(getInput("day24/input.txt")), "")
   }
