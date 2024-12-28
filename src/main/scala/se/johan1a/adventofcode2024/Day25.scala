6ffaf9c78de5e4619a217a10a89e596dbeaa185b
Johan Andersson <johan.peter.andersson@gmail.com>
2024-12-28 23:11:20 +0100

    Day 25

--- src/main/scala/se/johan1a/adventofcode2024/Day25.scala
+++ src/main/scala/se/johan1a/adventofcode2024/Day25.scala
@@ -1,9 +1,60 @@
 package se.johan1a.adventofcode2024
 
+import se.johan1a.adventofcode2024.Utils.split
+
 object Day25:
 
-  def part1(input: Seq[String]): Int =
-    -1
+  case class Lock(heights: Seq[Int])
+  case class Key(heights: Seq[Int])
 
-  def part2(input: Seq[String]): Int =
-    -1
+  def part1(input: Seq[String]): Int =
+    val (locks, keys) = parse(input)
+    var sum = 0
+    keys.foreach { key =>
+      locks.foreach { lock =>
+        if fits(key, lock) then
+          sum += 1
+      }
+    }
+    sum
+
+  private def fits(key: Key, lock: Lock) =
+    key.heights.indices.forall { i =>
+      key.heights(i) + lock.heights(i) <= 5
+    }
+
+  private def parse(input: Seq[String]) =
+    val splitted = split(input)
+    val objects = splitted.map { lines =>
+      if lines.head == "#####" then
+        var heights = Seq[Int]()
+        var height = 0
+        var i = 0
+        while i < lines.head.size do
+          var j = 1
+          while j < lines.size do
+            if lines(j).charAt(i) == '#' then
+              height += 1
+            j += 1
+          heights = heights :+ height
+          height = 0
+          i += 1
+        Lock(heights)
+      else
+        var heights = Seq[Int]()
+        var height = 0
+        var i = 0
+        while i < lines.head.size do
+          var j = 1
+          while j < lines.size - 1 do
+            if lines(j).charAt(i) == '#' then
+              height += 1
+            j += 1
+          heights = heights :+ height
+          height = 0
+          i += 1
+        Key(heights)
+    }
+    val locks = objects.collect { case l: Lock => l }
+    val keys = objects.collect { case k: Key => k }
+    (locks, keys)
--- src/test/scala/se/johan1a/adventofcode2024/Day25Test.scala
+++ src/test/scala/se/johan1a/adventofcode2024/Day25Test.scala
@@ -4,10 +4,14 @@ import se.johan1a.adventofcode2024.TestInputUtil.getInput
 
 class Day25Test extends munit.FunSuite:
 
+  test("Part 1 test") {
+    assertEquals(Day25.part1(getInput("day25/test.txt")), 3)
+  }
+
   test("Part 1") {
     assertEquals(Day25.part1(getInput("day25/input.txt")), -1)
   }
-
-  test("Part 2") {
-    assertEquals(Day25.part2(getInput("day25/input.txt")), -1)
-  }
+//
+//  test("Part 2") {
+//    assertEquals(Day25.part2(getInput("day25/input.txt")), -1)
+//  }
